1adbab33fa01c379e4d5b86908e1250a7d40bbd3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 00:12:54 -0300

    feat: ensure LoginForm renders e-mail input correctly

--- src/components/LoginForm/LoginForm.elements.tsx
+++ src/components/LoginForm/LoginForm.elements.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
-export const BaseDiv = styled.div`
-  padding: 15px;
-  width: 100%;
-  cursor: pointer;
+export const LoginSection = styled.section`
+  height: 100vh;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 `;
+
+export const SectionForm = styled.form`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LoginLabel = styled.label``;
+
+export const LoginInput = styled.input``;
