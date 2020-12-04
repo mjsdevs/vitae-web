d4c3174a96e1da51fd337ef25a1173f725ed8fe4
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 12:34:24 -0300

    feat: style Sign In button

--- src/components/LoginForm/LoginForm.elements.tsx
+++ src/components/LoginForm/LoginForm.elements.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LoginSection = styled.section`
   height: 100vh;
-  width: 500px;
+  min-width: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -13,6 +13,8 @@ export const SectionForm = styled.form`
   height: 300px;
   display: flex;
   flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
 `;
 
 export const LoginLabel = styled.label`
@@ -50,7 +52,23 @@ export const LoginInput = styled.input<InputProps>`
   }
 `;
 
-export const SignInButton = styled.button``;
+export const SignInButton = styled.button`
+  width: 150px;
+  height: 50px;
+  background-color: ${(props) => props.theme.colors.purple};
+  font: ${(props) => props.theme.fonts.smallText};
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.white};
+  border-radius: 5px;
+  border: none;
+  box-shadow: 3px, 3px, 5px rgba(0, 0, 0, 0.25);
+
+  :hover {
+    background-color: ${(props) => props.theme.colors.hoverPurple};
+  }
+
+  transition: background-color 0.1s ease-in;
+`;
 
 SectionForm.displayName = "form";
 LoginLabel.displayName = "label";
