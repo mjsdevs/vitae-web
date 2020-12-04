7873ef6de86f83c00c0e25119bcb99973af523f3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 15:51:45 -0300

    chore: add assets to project

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as LoginLogo } from './logo-vittae-colored.svg';
+export { ReactComponent as KeyIcon } from './key-password.svg';
+export { ReactComponent as UserIcon } from './fa-user-circle-normal.svg';
+export { ReactComponent as EyeIcon } from './fasfa-eye.svg';
