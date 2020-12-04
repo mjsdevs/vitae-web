5e3921fbafff1310c04d32633d14e24ceaa3dab2
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 12:12:21 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
+import GlobalStyle from "./assets/styles/global";
+import theme from "./assets/styles";
 import { LoginForm } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm handle={(e) => console.log("oi")} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <LoginForm
+          handle={(e) => {
+            e.preventDefault();
+            console.log("oi");
+          }}
+        />
+      </ThemeProvider>
     </div>
   );
 }
