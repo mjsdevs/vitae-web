2d45433b2191d574e6fb9181b24fc68eb473ec2e
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 00:12:54 -0300

    feat: ensure LoginForm renders e-mail input correctly

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import {
+  LoginSection,
+  SectionForm,
+  LoginLabel,
+  LoginInput,
+} from "./LoginForm.elements";
 
 const LoginForm: React.FC = () => {
   return (
-    <p>Dale Boy!</p>
+    <LoginSection>
+      <SectionForm>
+        <LoginLabel htmlFor="email-input">E-mail</LoginLabel>
+        <LoginInput id="email-input" />
+      </SectionForm>
+    </LoginSection>
   );
 };
 
