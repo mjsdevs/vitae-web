3bb431a94620b72f3ceeb035d6a09c175b3de4f9
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 14:34:39 -0300

    feat: ensure LoginForm renders Sign Up anchor with correct text

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -6,6 +6,8 @@ import {
   ActionAnchor,
   MainActionsContainer,
   SignInButton,
+  SignUpContainer,
+  SignUpText,
 } from "./LoginForm.elements";
 import LoginInput from "../LoginInput/LoginInput";
 import { LoginFormSignature } from "./protocols";
@@ -15,12 +17,21 @@ const LoginForm: React.FC<LoginFormSignature> = ({ handle }) => {
     <LoginSection>
       <SectionForm onSubmit={handle} noValidate>
         <LoginInput labelName="Email" identifier="email-input" type="email" />
-        <LoginInput labelName="Password" identifier="password-input" type="password" />
+        <LoginInput
+          labelName="Password"
+          identifier="password-input"
+          type="password"
+        />
 
         <MainActionsContainer>
           <ActionAnchor>Forgot Password?</ActionAnchor>
           <SignInButton type="submit">Sign In</SignInButton>
         </MainActionsContainer>
+
+        <SignUpContainer>
+          <SignUpText>Don't have an account?</SignUpText>
+          <ActionAnchor>Sign Up</ActionAnchor>
+        </SignUpContainer>
       </SectionForm>
     </LoginSection>
   );
