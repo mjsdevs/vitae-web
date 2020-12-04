f3128a4fa76a883bf8491e434c255bf4df84bc04
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 13:36:06 -0300

    test: ensure LoginInput renders placeholder with correct text

--- src/components/LoginInput/LoginInput.spec.tsx
+++ src/components/LoginInput/LoginInput.spec.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import LoginInput from './LoginInput';
-import { LoginInputProps } from './protocols';
+import React from "react";
+import { shallow } from "enzyme";
+import LoginInput from "./LoginInput";
+import { LoginInputProps } from "./protocols";
 
 const setup = () => {
   const props: LoginInputProps = {
-    labelName: "E-mail",
-    type: "email"
+    labelName: "Email",
+    type: "email",
   };
 
   return {
@@ -15,14 +15,19 @@ const setup = () => {
   };
 };
 
-describe('LoginInput test suit', () => {
-  it('Should render without errors', () => {
+describe("LoginInput test suit", () => {
+  it("Should render without errors", () => {
     const { wrapper } = setup();
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it('Should render a label with correct text', () => {
+  it("Should render a label with correct text", () => {
     const { wrapper } = setup();
-    expect(wrapper.find('label').text()).toEqual("E-mail");
+    expect(wrapper.find("label").text()).toEqual("Email");
+  });
+
+  it("Should render an input with correct placeholder", () => {
+    const { wrapper } = setup();
+    expect(wrapper.find("input").props().placeholder).toEqual("Email");
   });
 });
