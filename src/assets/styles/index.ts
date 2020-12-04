1341181d20bd8b369a2c8bb7332734d9bafe5f89
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 12:12:21 -0300

    feat: add global styles

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    purple: '#6B3EF4',
+    hoverPurple: '#5F37DB',
+    grayBorder: '#828282',
+    red: '#D40000'
+  },
+
+  fonts: {
+    sectionHeader: 'normal normal normal 26px "Montserrat", sans-serif',
+    sectionSubtitle: 'normal normal normal 21px "Montserrat", sans-serif',
+    normalText: 'normal normal normal 16px "Montserrat", sans-serif',
+    smallText: 'normal normal normal 13px "Montserrat", sans-serif',
+  }
+}
---
+++ src/assets/styles/styled.d.ts
@@ -0,0 +1,21 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string;
+      white: string;
+      purple: string;
+      hoverPurple: string;
+      grayBorder: string;
+      red: string;
+    };
+
+    fonts: {
+      sectionHeader: string;
+      sectionSubtitle: string;
+      normalText: string;
+      smallText: string;
+    }
+  }
+}
