d6eb3cd03331746d903563c1841267967e92be17
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 13:31:11 -0300

    test: ensure LoginInput renders label with correct value

--- src/components/LoginInput/LoginInput.spec.tsx
+++ src/components/LoginInput/LoginInput.spec.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import LoginInput from './LoginInput';
+import { LoginInputProps } from './protocols';
 
 const setup = () => {
-  const props = {};
+  const props: LoginInputProps = {
+    labelName: "E-mail",
+    type: "email"
+  };
 
   return {
     props,
@@ -12,8 +16,13 @@ const setup = () => {
 };
 
 describe('LoginInput test suit', () => {
-  it('renders without error', () => {
+  it('Should render without errors', () => {
     const { wrapper } = setup();
     expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('Should render a label with correct text', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find('label').text()).toEqual("E-mail");
   });
 });
