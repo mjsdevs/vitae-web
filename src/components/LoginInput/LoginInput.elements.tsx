43bb173439533b1e674aab7dae7b4d544f8b15ac
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-04 16:05:33 -0300

    feat: add icon to LoginInput

--- src/components/LoginInput/LoginInput.elements.tsx
+++ src/components/LoginInput/LoginInput.elements.tsx
@@ -8,12 +8,16 @@ interface InputProps {
   isWrong: boolean;
 }
 
+export const LoginInputContainer = styled.div`
+  display: flex;
+`;
+
 export const LoginInputField = styled.input<InputProps>`
   height: 50px;
   min-width: 275px;
-  width: 400px;
+  width: 350px;
   padding: 15px;
-  border-radius: 5px;
+  border-radius: 0px 5px 5px 0px;
   border: 1px solid
     ${(props) => {
       const borderColor = props.isWrong
@@ -57,6 +61,53 @@ export const LoginInputField = styled.input<InputProps>`
     }};
 
     font: ${(props) => props.theme.fonts.smallText};
+  }
+`;
+
+export const IconContainer = styled.div<InputProps>`
+  height: 50px;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px 0px 0px 5px;
+  border-top: 1px solid
+    ${(props) => {
+      const borderColor = props.isWrong
+        ? props.theme.colors.red
+        : props.theme.colors.grayBorder;
+
+      return borderColor;
+    }};
+
+  border-bottom: 1px solid
+    ${(props) => {
+      const borderColor = props.isWrong
+        ? props.theme.colors.red
+        : props.theme.colors.grayBorder;
+
+      return borderColor;
+    }};
+
+  border-left: 1px solid
+    ${(props) => {
+      const borderColor = props.isWrong
+        ? props.theme.colors.red
+        : props.theme.colors.grayBorder;
+
+      return borderColor;
+    }};
+
+  svg {
+    path {
+      fill: ${(props) => {
+        const borderColor = props.isWrong
+          ? props.theme.colors.red
+          : props.theme.colors.grayBorder;
+
+        return borderColor;
+      }};
+    }
   }
 `;
 
