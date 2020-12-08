962c2a92d6898c85eec468de1dfaa56344347fe0
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-12-08 10:28:02 -0300

    feat: ensure LoginRightSection shows a Fork me on GitHub ribbon

---
+++ src/components/LoginRightSection/LoginRightSection.elements.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SectionContainer = styled.section`
+  min-width: 780px;
+  max-width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.purple};
+`;
--- src/components/LoginRightSection/LoginRightSection.tsx
+++ src/components/LoginRightSection/LoginRightSection.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+
+import { SectionContainer } from "./LoginRightSection.elements";
 
 const LoginRightSection: React.FC = () => {
   return (
-    <p>Dale Boy!</p>
+    <SectionContainer>
+      <a href="https://github.com/you">
+        <img
+          loading="lazy"
+          width="149"
+          height="149"
+          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+          className="attachment-full size-full"
+          alt="Fork me on GitHub"
+          data-recalc-dims="1"
+        />
+      </a>
+    </SectionContainer>
   );
 };
 
